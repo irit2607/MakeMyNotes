7176b13e748916005ab12e1fe9464509202b8713
irit2607 <67689773+irit2607@users.noreply.github.com>
2021-01-11 12:25:33 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -12,9 +12,14 @@ addbtn.addEventListener("click", function (e) {
   } else {
     notesObj = JSON.parse(notes);
   }
-  notesObj.push(addTxt.value);
+  let myobj ={
+    title: addTitle.value,
+    text: addTxt.value
+  }
+  notesObj.push(myobj);
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
+  addTitle.value = "";
   //   console.log(notesObj);
   showNotes();
 });
@@ -32,8 +37,8 @@ function showNotes() {
     html += `
             <div class="noteCard my-2 mx-2 card" style="width: 21rem;">
                     <div class="card-body">
-                        <h5 class="card-title">Note ${index + 1}</h5>
-                        <p class="card-text"> ${element}</p>
+                        <h5 class="card-title">${index + 1}.${element.title}</h5>
+                        <p class="card-text"> ${element.text}</p>
                         <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-outline-danger" style="font-size: 1em;">Delete Note</button>
                     </div>
                 </div>`;
